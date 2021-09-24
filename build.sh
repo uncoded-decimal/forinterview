011c21ac35f13ab95c11057b86b44f26ce8720c2
andi <nigam.aditya13@gmail.com>
2021-09-24 15:25:44 +0530

    variant push

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 flutter clean
 flutter pub get
+flutter build apk --profile
 flutter build apk --release
+mv /home/dr_decimal/Desktop/forinterview/build/app/outputs/flutter-apk/app-profile.apk /home/dr_decimal/Desktop/forinterview/build/app/outputs/flutter-apk/dice_app-profile.apk
 mv /home/dr_decimal/Desktop/forinterview/build/app/outputs/flutter-apk/app-release.apk /home/dr_decimal/Desktop/forinterview/build/app/outputs/flutter-apk/dice_app.apk
 xdg-open /home/dr_decimal/Desktop/forinterview/build/app/outputs/flutter-apk/
